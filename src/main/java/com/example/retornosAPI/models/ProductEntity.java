7e13bde2d2f8114d690e34a17b0b619f5d30b5b4
Lara Oliveira <lara.oliveira@zup.com.br>
2025-01-26 18:50:24 -0300

    faz validação dos atributos nome, preço, quantidade em estoque e categoria e adiciona seus getters e setters

--- src/main/java/com/example/retornosAPI/models/ProductEntity.java
+++ src/main/java/com/example/retornosAPI/models/ProductEntity.java
@@ -1,9 +1,7 @@
 package com.example.retornosAPI.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class ProductEntity {
@@ -11,41 +9,70 @@ public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message = "O nome não pode ser vazio.")
+    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres.")
     private String name;
+
+    @DecimalMin(value = "0.01", message = "O valor deve ser maior que 0!")
     private Double price;
+
+    @NotNull(message = "A quantidade não pode ser nula.")
+    @Min(value = 0, message = "A quantidade em estoque deve ser maior ou igual a 0.")
+    private int stockQuantity;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
 
     public ProductEntity() {
     }
 
-    public ProductEntity(Long id, String name, Double price) {
+    public ProductEntity(Long id, String name, Double price, int stockQuantity, Category category) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.category = category;
+    }
+
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Double getPrice() {
         return price;
     }
 
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
